5d97e12e129a224763f3850f36a38546310c6252
Daniel <burvaldaniel@gmail.com>
2026-03-27 14:29:55 +0100

    initial

---
+++ src/main/java/cz/networkninjas/Main.java
@@ -0,0 +1,10 @@
+package cz.networkninjas;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Multiplication multiplication = new Multiplication(27);
+        multiplication.multiply();
+    }
+}
---
+++ src/main/java/cz/networkninjas/Multiplication.java
@@ -0,0 +1,36 @@
+package cz.networkninjas;
+
+public class Multiplication {
+    int number;
+    int level = 1;
+
+    private Multiplication() {
+
+    }
+
+    public Multiplication(int number) {
+        this.number = number;
+    }
+
+    public int multiply() {
+        return multiplication(this.number, this.level);
+    }
+
+    private int multiplication(int number, int level) {
+        System.out.println("Number:" + number + " level:" + level);
+        int multipliedNumber = getMultiplicationOfNumbers(number);
+        if (multipliedNumber > 9) {
+            level++;
+            return multiplication(multipliedNumber, level);
+        } else return level;
+    }
+
+    private int getMultiplicationOfNumbers(int number) {
+        int multiplication = 1;
+        while (number % 10 > 0) {
+            multiplication *= number % 10;
+            number = number / 10;
+        }
+        return multiplication;
+    }
+}
